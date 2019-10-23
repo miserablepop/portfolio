e03f3add1c3df3e3a6487c66b7e40e9f116a3798
JC Lenz <jc@miserablepop.com>
2019-10-23 12:21:53 -0700

    Added portfolio carousel and jquery for carousel

--- assests/javascript/init.js
+++ assests/javascript/init.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.carousel').carousel();
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+      });
   });
+
+
        
